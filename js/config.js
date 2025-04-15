5e6d0bdd8c5857515faf60b431feaac5a3efd7d0
atillakhalilli0 <atilla.xalilli@gmail.com>
2025-04-15 19:11:01 +0400

    com22

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 const BASE_URL = {
-    GET: "https://papa-data-byatilla.vercel.app",
-    POST: "https://papa-data-byatilla.vercel.app",
-    PUT: "https://papa-data-byatilla.vercel.app",
-    DELETE: "https://papa-data-byatilla.vercel.app",
+    GET: "https://papadata-ovig.onrender.com/package.json",
+    POST: "https://papadata-ovig.onrender.com/package.json",
+    PUT: "https://papadata-ovig.onrender.com/package.json",
+    DELETE: "https://papadata-ovig.onrender.com/package.json",
 }
 
 export default BASE_URL
