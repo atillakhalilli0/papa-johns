f185d8502cdf29191a16b64acb44923a364cb510
atillakhalilli0 <atilla.xalilli@gmail.com>
2025-04-15 21:07:15 +0400

    url change 23

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 const BASE_URL = {
-    GET: "https://papadata-ovig.onrender.com/package.json",
-    POST: "https://papadata-ovig.onrender.com/package.json",
-    PUT: "https://papadata-ovig.onrender.com/package.json",
-    DELETE: "https://papadata-ovig.onrender.com/package.json",
+    GET: "https://atillapapa.onrender.com",
+    POST: "https://atillapapa.onrender.com",
+    PUT: "https://atillapapa.onrender.com",
+    DELETE: "https://atillapapa.onrender.com",
 }
 
 export default BASE_URL
